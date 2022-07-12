0de473934583a2c09b74f2acdbbca7867a2a90b7
SSAFY.Hong <iccack70@gmail.com>
2022-07-12 18:02:52 +0900

    java_ws_01_3 upload

---
+++ src/com/ssafy/ws/step3/DigitTest1.java
@@ -0,0 +1,13 @@
+package com.ssafy.ws.step3;
+
+/**
+ * 직각삼각형 모양의 숫자 출력하는 클래스
+ */
+public class DigitTest1 {
+
+	public static void main(String[] args) {
+		//코드를 작성하세요.
+		
+	}
+
+}
---
+++ src/com/ssafy/ws/step3/DigitTest2.java
@@ -0,0 +1,12 @@
+package com.ssafy.ws.step3;
+
+/**
+ * 모래시계 모양의 숫자 출력하는 클래스
+ */
+public class DigitTest2 {
+
+	public static void main(String[] args) {
+		//코드를 작성하세요.
+
+	}
+}
