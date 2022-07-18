cb213b656b9a350206d14d1ea740b6c6dcaaea90
전은수 <javanism@hanmail.net>
2022-07-18 09:05:47 +0900

    Update Hello.java, src/com/ssafy/ws/step3/Hello.java

---
+++ src/com/ssafy/ws/step3/Hello.java
@@ -0,0 +1,12 @@
+
+package com.ssafy.ws.step3;
+
+/**
+ * 직각삼각형 모양의 숫자 출력하는 클래스
+ */
+public class DigitTest1 {
+
+	public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
