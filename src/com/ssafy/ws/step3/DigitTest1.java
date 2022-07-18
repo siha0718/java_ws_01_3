d86cac31a987988b1b2ce4d2c9b60cca43fd933d
임경혜 <khim98@naver.com>
2022-07-18 10:04:54 +0900

    Update src/com/ssafy/ws/step3/DigitTest1.java, src/com/ssafy/ws/step3/DigitTest2.java

--- src/com/ssafy/ws/step3/DigitTest1.java
+++ src/com/ssafy/ws/step3/DigitTest1.java
@@ -8,18 +8,7 @@ public class DigitTest1 {
 	public static void main(String[] args) {
 		//코드를 작성하세요.
 		//test
-		int count = 0;
-
-		for (int i = 0; i < 5; i++) {
-			for (int j = 0; j < 5; ++j) {
-				if (j < i) {
-					System.out.printf("%3s", " ");
-				} else {
-					System.out.prinf("%3d", ++count)
-				}
-			}
-			System.out.println();
-		}
+		
 	}
 
 }
